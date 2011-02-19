4fc492b35ebe510f73e541daad74c879240f250b
dhatch387 <dhatch387@gmail.com>
2011-02-19 01:47:19 -0500

    fixed various typos

--- bukkitmanager/download.py
+++ bukkitmanager/download.py
@@ -10,9 +10,9 @@ def download(args):
     global options
     parser = optparse.OptionParser(usage="%prog [options] VERSION_NUMBER")
     parser.add_option("-v", "--verbose", action = "store_true", help = "print debug data")
-    (options, args) = parser.parse_args()
+    (options, args) = parser.parse_args(args)
     verbose("Args are: %s " % args)
-    if args != 1:
+    if len(args) != 1:
         print "Error: Only accept 1 argument. Please supply the version to download."
         parser.print_help()
         exit()
@@ -22,7 +22,7 @@ def download(args):
         print "Error: Must supply a numrical value for VERSION_NUMBER!"
         parser.print_help()
         exit()
-    f = open(sys.path.split("/")[1:-1]+"/craftbukkit-%d.jar" % download_number, "w")
+    f = open("/".join(sys.path[0].split("/")[0:-1])+"/craftbukkit-%d.jar" % download_number, "w")
     f.write(urllib2.urlopen("http://bamboo.lukegb.com/browse/BUKKIT-CRAFTBUKKIT-%d/artifact/JOB1/CraftBukkit-JAR/craftbukkit-0.0.1-SNAPSHOT.jar" % download_number,None).read())
     f.close()
 
