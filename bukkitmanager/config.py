5ec45310fe0f1e2419fc2eed73a1c2fe7486892a
dhatch387 <dhatch387@gmail.com>
2011-02-25 18:50:52 -0500

    moved config loading to a separate file

---
+++ bukkitmanager/config.py
@@ -0,0 +1,20 @@
+import sys, os.path
+
+#name
+name = sys.path[0].split("/")[-1]
+#set testing server
+if name.find('test_') != -1:
+    test_server = True
+else:
+    test_server = False
+    
+#users
+users = list()
+try:
+    ops_file = open(os.path.join(sys.path[0],'ops.txt'), 'r')
+    for line in ops_file:
+        users += line
+    ops_file.close()
+except IOError:
+    #default fall through
+    users = ['dhatch','pconzone','beyring']
