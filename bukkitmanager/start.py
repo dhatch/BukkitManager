98a4714f7f018c66102a41cd17ac74d46ee6da9e
dhatch387 <dhatch387@gmail.com>
2011-02-25 19:00:19 -0500

    setup start.py to read from config

--- bukkitmanager/start.py
+++ bukkitmanager/start.py
@@ -51,7 +51,7 @@ def start(args):
      usage=bcolors.OKGREEN+"%prog start"+bcolors.ENDC+" [options] FILENAME [min_memory (MB)] [max_memory (MB)]")
     parser.add_option('-v', '--verbose', action='store_true', help='print debug data')
     #later replace default value here with config
-    parser.add_option('-s', '--screen-name', action='store', help='the name of the screen session', default='minecraft_tester')
+    parser.add_option('-s', '--screen-name', action='store', help='the name of the screen session', default=config.name)
     (options, args) = parser.parse_args(args)
     verbose("Args are: %s " % args)
     if len(args) < 1:
@@ -59,7 +59,6 @@ def start(args):
         parser.print_help()
         exit()
     ##READY TO START SERVER
-    #defaults here should depend on config, eventually add in setting based on input
     try:
         min_memory = args[1]
     except IndexError:
@@ -67,7 +66,10 @@ def start(args):
     try:
         max_memory = args[2]
     except IndexError:
-        max_memory = 512
+        if config.test_server:
+            max_memory = 512
+        else: 
+            max_memory = 2048
     file_n = args[0]
     verbose(bcolors.OKGREEN+"Running:"+bcolors.ENDC+" screen -mS %s java -Xms%dM -Xmx%dM -Xincgc -jar %s nogui" % (options.screen_name, min_memory, max_memory, file_n))
     screen_child = pexpect.spawn("screen",["-m","-S",options.screen_name, "java","-Xms%dM" % min_memory,"-Xmx%dM" % max_memory,"-Xincgc","-jar",'%s' % file_n,"nogui"])
@@ -103,9 +105,7 @@ def start(args):
                 print bcolors.OKGREEN+"Server up and running. Setting up screen..."+bcolors.ENDC            
                 os.system("screen -S %s -X multiuser on" % options.screen_name)
                 #add users to screen
-                #eventually users to add should be read from config
-                users = ['dhatch', 'pconzone', 'beyring']
-                for u in users:
+                for u in config.users:
                     os.system("screen -S %s -X acladd %s" % (pwd.getpwuid(os.getuid())[0]+"/"+options.screen_name, u))
                 print bcolors.OKGREEN+"Started sucessfully with session name %s%s%s!" % (bcolors.OKBLUE, options.screen_name, bcolors.OKGREEN)+bcolors.ENDC\
                 + "\nConnect with %smanage.py connect" % bcolors.OKBLUE+bcolors.ENDC
