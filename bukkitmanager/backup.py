82bb0794fbe8e2ad0cc1458e47b5c74b2109360d
dhatch387 <dhatch387@gmail.com>
2011-03-13 01:02:01 -0500

    finished backup

--- bukkitmanager/backup.py
+++ bukkitmanager/backup.py
@@ -1,30 +1,72 @@
 import optparse
+import shutil
+import sys
+import os
+import datetime
+import zipfile
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
 
-    def disable(self):
-        self.HEADER = ''
-        self.OKBLUE = ''
-        self.OKGREEN = ''
-        self.WARNING = ''
-        self.FAIL = ''
-        self.ENDC = ''
+	def disable(self):
+		self.HEADER = ''
+		self.OKBLUE = ''
+		self.OKGREEN = ''
+		self.WARNING = ''
+		self.FAIL = ''
+		self.ENDC = ''
 
-def verbose(string):    
-    global options
-    if options.verbose: print string
+def verbose(string):	
+	global options
+	if options.verbose: print string
 
+#this is convenient
+def zipper(dir, zip_file):
+    zip = zipfile.ZipFile(zip_file, 'w', compression=zipfile.ZIP_DEFLATED)
+    root_len = len(os.path.abspath(dir))
+    for root, dirs, files in os.walk(dir):
+    	#print root, dirs, files
+        archive_root = os.path.abspath(root)[root_len:]
+        for f in files:
+            fullpath = os.path.join(root, f)
+            archive_name = os.path.join(archive_root, f)
+            print f
+            zip.write(fullpath, archive_name, zipfile.ZIP_DEFLATED)
+    zip.close()
+    return zip_file
+    
 def backup(args):
-    global options
-    parser = optparse.OptionParser(usage="%prog [options] ")
-    parser.add_option("-v", "--verbose", action = "store_true", help = "print debug data")
-    parser.add_option("-f", "--file-list", help = "Enter a list of additional files you want to backup.")
-    parser.add_option("-l", "--location", help = "Enter a directory to save the file(s) to.")
-    (options, args) = parser.parse_args(args)
-    verbose("Args are: %s " % args)
+	global options
+	parser = optparse.OptionParser(usage="%prog [options] ")
+	parser.add_option("-v", "--verbose", action = "store_true", help = "print debug data")
+	parser.add_option("-f", "--file-list", help = "Enter a list of additional files you want to backup.")
+	parser.add_option("-l", "--location", help = "Enter a directory to save the file(s) to.")
+	(options, args) = parser.parse_args(args)
+	verbose("Args are: %s " % args)
+	## setting up the location for the world file to be saved to
+	
+	# split the list into strings
+	location = sys.path[0].split("/")
+	del location[-1]
+	# create a seperate path for finding the world file
+	world_location = "/".join(location)
+	# make the strings back into a list for location to save the backup world
+	backup_location = os.path.join(world_location,"backups","world")
+	# change the dir to find the world dir
+	os.chdir(world_location)
+	#find the current time
+	today = datetime.datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
+	print today
+	shutil.copytree("test_world", os.path.join(backup_location,today))
+	#ZIP IT UP BITCH
+	os.chdir(backup_location)
+	#YEA MAN
+	zipper(today,today+".zip")
+	
+if __name__ == "__main__":
+	backup(sys.argv)
