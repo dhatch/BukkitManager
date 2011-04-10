a4c7eb55a972e62b4c7008870d17147d990d60e2
dhatch387 <dhatch387@gmail.com>
2011-04-09 22:10:21 -0400

    fixed issue with starting a secondary screen

--- bukkitmanager/restart.py
+++ bukkitmanager/restart.py
@@ -31,16 +31,7 @@ def restart(args):
     parser.add_option('-v', '--verbose', action='store_true', help='print debug data')
     (options, args) = parser.parse_args(args)
     verbose("Args are: %s " % args)
-    screen_child = pexpect.spawn("screen -S %s" % config.readScreenName())
     print bcolors.OKBLUE, "Restarting", bcolors.ENDC
-    while True:
-        try:
-            screen_child.expect("no screen", timeout=3)
-        except pexpect.TIMEOUT:
-            stop.stop([])
-            ##SERVER STILL NEEDS RESTARTING HERE (FIRST CONFIG NEEDS TO BE ADVANCED TO STORE THE FILE NAME AND THE START PARAMS OF PREVIOUS START)
-            break
-        except pexpect.EOF, e:
-            print bcolors.FAIL + "Process unexpectedly terminated\n%s" % e+bcolors.ENDC
-            break
-        start.start(config.readStartParams())
+    stop.stop([])
+    ##SERVER STILL NEEDS RESTARTING HERE (FIRST CONFIG NEEDS TO BE ADVANCED TO STORE THE FILE NAME AND THE START PARAMS OF PREVIOUS START)
+    start.start(config.readStartParams())
